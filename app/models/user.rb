f918f6f255a61e2dfc2833be3db5d2013788b4b0
Rolen Le <rolentle@gmail.com>
2014-02-10 22:06:45 -0700

    set twitter keys and added from omniauth to twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+    def self.from_omniauth(auth)
+      user = User.find_or_create_by(
+	uid:      auth["uid"]
+      )
+      user.update_attributes(
+	name:          auth["info"]["nickname"],
+	provider: auth["provider"],
+	avatar_url:    auth["info"]["image"],
+	access_token:  auth["credentials"]["token"],
+	access_secret: auth["credentials"]["secret"]
+      )
+      user
+    end
 end
