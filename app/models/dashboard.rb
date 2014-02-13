1920362846b58841cee98fb87bcd7e3ec537add4
Rolen Le <rolentle@gmail.com>
2014-02-12 23:11:10 -0700

    Added Tweets per Minute

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -6,10 +6,26 @@ class Dashboard
   end
 
   def last_24_hours_batch_sizes
-    Time.zone = 'Eastern Time (US & Canada)'
     last_24_hours_batch_times.map do |time|
       [time, Status.where(batch_time: time).count]
     end
   end
 
+  def last_24_hours_tweet_times_in_5mins
+    sent_ats = Status.pluck(:sent_at).uniq.select do |time|
+      time > (DateTime.now - 86400)
+    end.sort
+
+    sent_ats.map do |time|
+      time = time - time.sec
+    end.uniq
+  end
+
+  def last_24_hours_tweets_per_5min
+    last_24_hours_tweet_times_in_5mins.map do |time|
+      [time, Status.where("sent_at >= :start_time AND sent_at < :end_time",
+			  {start_time: time,
+                           end_time: time + 60}).count]
+    end
+  end
 end
