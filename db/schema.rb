5e6ba12f436a45284eb40d15cb1d607030af4473
Rolen Le <rolentle@gmail.com>
2014-02-12 19:06:06 -0700

    created statuses model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211053225) do
+ActiveRecord::Schema.define(version: 20140213020513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "statuses", force: true do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.datetime "sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "provider"
