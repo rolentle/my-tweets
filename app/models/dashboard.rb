6c705db5305e87003c321c1196a8559db0e2ccde
Rolen Le <rolentle@gmail.com>
2014-02-14 08:04:53 -0700

    optmization

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -6,9 +6,10 @@ class Dashboard
   end
 
   def last_24_hours_batch_sizes
-  @l24hrsbz ||=  last_24_hours_batch_times.map do |time|
-      [time, Status.where(batch_time: time).count]
-    end
+  #@l24hrsbz ||=  last_24_hours_batch_times.map do |time|
+  #    [time, Status.where(batch_time: time).count]
+  # end
+    @batch_time||= Status.group(:batch_time).count
   end
 
   def last_24_hours_tweet_times_in_5mins
@@ -22,10 +23,11 @@ class Dashboard
   end
 
   def last_24_hours_tweets_per_5min
-    @l24hrs_5min ||= last_24_hours_tweet_times_in_5mins.map do |time|
-      [time, Status.where("sent_at >= :start_time AND sent_at < :end_time",
-			  {start_time: time,
-                           end_time: time + 60}).count]
-    end
+    #@l24hrs_5min ||= last_24_hours_tweet_times_in_5mins.map do |time|
+    #  [time, Status.where("sent_at >= :start_time AND sent_at < :end_time",
+    #			  {start_time: time,
+    #                       end_time: time + 60}).count]
+    #end
+    @tweet_time||= Status.group(:sent_at).order(:sent_at).count
   end
 end
